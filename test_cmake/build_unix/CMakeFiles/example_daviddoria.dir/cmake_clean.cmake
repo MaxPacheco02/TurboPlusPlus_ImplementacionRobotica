979128e94250e9e524089bd7ab3a663362993eea
DemianMArin <demianmarin03@gmail.com>
2024-04-24 18:30:17 -0600

    Addding calibration opencv cpp

---
+++ test_cmake/build_unix/CMakeFiles/example_daviddoria.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_daviddoria.dir/example_daviddoria.cpp.o"
+  "CMakeFiles/example_daviddoria.dir/example_daviddoria.cpp.o.d"
+  "example_daviddoria"
+  "example_daviddoria.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example_daviddoria.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test_cmake/build_unix/CMakeFiles/test_extrinsic_camera_params.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_extrinsic_camera_params.dir/test_extrinsic_camera_params.cpp.o"
+  "CMakeFiles/test_extrinsic_camera_params.dir/test_extrinsic_camera_params.cpp.o.d"
+  "test_extrinsic_camera_params"
+  "test_extrinsic_camera_params.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_extrinsic_camera_params.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

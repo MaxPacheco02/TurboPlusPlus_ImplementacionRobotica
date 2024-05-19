b616105927dd1bf845b7706dbed99934dc49fbf8
Daniel Castañón <147120225+DanielCas815@users.noreply.github.com>
2024-05-18 22:53:41 -0600

    Add files via upload

--- Reto3_Vis/Draw_ax.py
+++ Reto3_Vis/Draw_ax.py
@@ -21,25 +21,22 @@ class Draw:
         self.ax.set_ylabel('Y')  
         self.ax.set_zlabel('Z')
         self.ax.view_init(30,45)
-    
-    def cal(self, T, R):
+        
+        # POSICION DE ORIGEN DE MUNDO
+        self.Pw = np.array([[0, 0, 0, 1]]).T
         # FRAME DE MUNDO
         self.Fw = np.array([[-62, 0, 0], [0, 62, 0], [0, 0, 62]])
-        
-        # POSICION DE FRAME
-        self.Pw = np.array([[0, 0, 0, 1]]).T
+    
+    def cal(self, T, R):
+        # MATRIZ HOMOGENEA
         H = np.concatenate((np.concatenate((R , T), axis=1),[[0,0,0,1]]), axis=0)
-        
         # PERFORM ROTATION
         self.Fw_r = R @ self.Fw
         # TRASLACION CON MATRIZ HOMOGENEA
         self.Pw_n = H @ self.Pw
-        #VOLTEAR EJE Z DEL REFERENCIAL DE CAMARA
+        #VOLTEAR EJE Z DEL REFERENCIAL DE CAMARA (CONVENCION)
         # self.Fw_r[2] = -1 * self.Fw_r[2] 
-
-
-
-
+    
     def draw_ax(self, pos, mag):
         self.ax.quiver(pos[0], pos[1], pos[2], mag[0][0], mag[1][0], mag[2][0], color='b') # X
         self.ax.quiver(pos[0], pos[1], pos[2], mag[0][1], mag[1][1], mag[2][1], color='r') # Y
